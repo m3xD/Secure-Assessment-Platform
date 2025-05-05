b32c43cf0b18a305ad2b69d606dec91685ad2f08
ciguliaz <ckazzgd@gmail.com>
2025-05-06 01:04:26 +0700

    ignore: formatting

--- src/services/questionsService.ts
+++ src/services/questionsService.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Question, QuestionData } from "../types/QuestionTypes";
 import { mainApi } from "../utils/AxiosInterceptor";
 
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { User } from "../types/UserTypes";
 import { authApi } from "../utils/AxiosInterceptor";
 
